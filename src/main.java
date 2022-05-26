8836d12ecf45439bdb939b3289cf56df4ce7ea29
Антон Чумаченко <anton4e@bk.ru>
2022-05-26 15:41:48 +0700

    HW2-1 java

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int x = 20;
+
+        int bonus = price / x;
+
+        System.out.println(bonus);
+
+    }
+}
