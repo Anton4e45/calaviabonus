b77ba41a556c0f55756fa74af8f0a01732883bae
Антон Чумаченко <anton4e@bk.ru>
2022-05-26 17:26:28 +0700

    HW3-1 java, модернизация

--- src/main.java
+++ src/main.java
@@ -1,12 +1,8 @@
 public class main {
-    public static void main(String[] args) {
-        int price = 5000;
-        int x = 20;
-
-        int bonus = price / x;
-
-        System.out.println("при стоимости билета: " + price + "руб.");
-        System.out.println("Будет начислено бонусных миль: " + bonus);
-
-    }
+        public static void main(String[] args) {
+            BonusMilesService service = new BonusMilesService();
+            int price = 10_000;
+            int miles = service.calculate(price);
+            System.out.println(miles);
+        }
 }
